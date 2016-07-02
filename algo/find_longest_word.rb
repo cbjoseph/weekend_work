a2f41f378cc6e40e75251bcd5a0588bb31d41eda
Caroline Joseph <carbroj@gmail.com>
2016-07-02 16:03:18 -0500

    Fixed find_longest_word.rb

--- algo/find_longest_word.rb
+++ algo/find_longest_word.rb
@@ -1,25 +1,17 @@
 # Complete the method called find_longest_word, which will accept a string as a parameter (usually a sentence), and return another string that will be the longest word in that sentence.
 
-class Sentence
-  def initialize(input)
-    @sentence = input
-  end
-
   def find_longest_word(sentence)
     index = 0
     longest_word = []
-    @sentence.split.each do |word|
+    sentence.split.each do |word|
       if word.length > index
         index = word.length
         longest_word = word
       end
     end
-    puts longest_word
+     return longest_word
   end
-end
 
-sentence = Sentence.new("OH my jiminy cricket I think I did it")
-sentence.find_longest_word
 
 # Driver code - don't touch anything below this line.
 puts "TESTING find_longest_word..."
